0cf1b30bfee7c5006598929796fbf21466e74c7e
Patricia <marie.lou.trips@gmail.com>
2026-05-21 00:07:43 +0200

    Display live weather data in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 import Header from "./Header"
+import {getTemperatureNow } from "./api/weather-openmeteo.ts"
 
 function App() {
-  const [temperature, setTemperature] = useState("")
+  const location = "Cologne"
+  const latitude = 50.94
+  const longitude = 6.96
+
+  const [temperature, setTemperature] = useState<number | null> (null);
+
+  useEffect(() => {
+    getTemperatureNow(latitude, longitude).then((temperatura: number) => {
+      setTemperature(temperatura)
+    });
+  }, []);
+
   return(
     <main>
       <h1>Weather Dashboard</h1>
-      <p>Tracking weather metrics in real time.</p>
-      <Header title="Temparature Overview"></Header>
-      <Header title="Sea Level Rise"></Header>
+      <p>Tracking weather metrics in real time.</p><br></br>
+      <Header location={location}></Header>
+
+      <h3>
+        {temperature === null ? "Loading data..." : `${temperature}°C`}
+      </h3>
+  
     </main>
   )
 }
