32566cfe33fed670a8e15b9a73100249e5594ece
Patricia <marie.lou.trips@gmail.com>
2026-05-21 00:54:36 +0200

    add dataset (a few hardcoded examples)

---
+++ src/data/cities.ts
@@ -0,0 +1,47 @@
+export const cities = [
+   {
+    name:"Cologne",
+    latitude: 50.94,
+    longitude: 6.96,
+  },
+    {
+    name:"Berlin", 
+    latitude: 52.52,
+    longitude: 13.41,
+  },
+    {
+  name:"Lisbon",
+    latitude: 38.72,
+    longitude: -9.14,
+  },
+    {
+    name:"Paris",
+    latitude: 48.86,
+    longitude: 2.35,
+  },
+    {
+    name:"São Paulo",
+    latitude: -23.55,
+    longitude: -46.63,
+  },
+    {
+    name:"Sydney",
+    latitude: -33.87,
+    longitude: 151.21,
+  },
+    {
+    name:"New York",
+    latitude: 40.71,
+    longitude: -74.01,
+  },
+    {
+    name: "Hong Kong",
+    latitude: 22.32,
+    longitude: 114.17,
+  },
+{
+    name:"Tokyo",
+    latitude: 35.68,
+    longitude: 139.69,
+  }
+]
