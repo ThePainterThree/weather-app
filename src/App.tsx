a8732a28f23bbc4f80fab7025c17e2b36ce72d27
Patricia <marie.lou.trips@gmail.com>
2026-05-22 17:21:02 +0200

    refactor App.tsx func to return more weather info instead of only the temperature

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import { useEffect, useState } from "react";
 import Header from "./Header";
-import { getTemperatureNow } from "./api/weather-openmeteo.ts";
+import { getWeatherNow, type WeatherNow } from "./api/weather-openmeteo.ts";
 import { cities } from "./data/cities.ts";
 import Dropdown from "./Dropdown.tsx";
 
+
 function App() {
   const [cityName, setCityName] = useState<string>("Cologne");
+  const [weather, setWeather] = useState<WeatherNow | null>(null);
+
   const location = cities.find((city) => city.name === cityName);
   if (!location) {
-    return <h5>Sorry, city not found. Try again later.</h5>;
+    return <h5>Sorry, city not found.</h5>;
   }
+
   const latitude = location.latitude;
   const longitude = location.longitude;
 
-  const [temperature, setTemperature] = useState<number | null>(null);
-
   useEffect(() => {
-    getTemperatureNow(latitude, longitude).then((temperatura: number) => {
-      setTemperature(temperatura);
-    });
+    setWeather(null)
+
+    getWeatherNow(latitude, longitude)
+      .then((weatherInfo) => {
+        setWeather(weatherInfo)
+    })
   }, [latitude, longitude]);
 
   return (
@@ -36,7 +41,7 @@ function App() {
       <br></br>
       <Header location={location.name}></Header>
       <br></br>
-      <h3>{temperature === null ? "Loading data..." : `${temperature}°C`}</h3>
+      <h3>{weather === null ? "Loading data..." : `${weather}°C`}</h3>
     </main>
   );
 }
