6f7c77a27e885d1da6e078ee6287d877de983569
Patricia <marie.lou.trips@gmail.com>
2026-05-22 16:40:35 +0200

    rename and refactor function to fetch more info from the api for a more complete weather report

--- src/api/weather-openmeteo.ts
+++ src/api/weather-openmeteo.ts
@@ -1,20 +1,37 @@
+type WeatherNow = {
+  temperature: number;
+  windSpeed: number;
+  humidity: number;
+  rain: number;
+};
 
+export async function getWeatherNow(
+  latitude: number,
+  longitude: number,
+): Promise<WeatherNow> {
+  const response = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,rain`,
+  );
 
+  if (!response.ok) {
+    throw new Error("Failed to display data. Please try again later.");
+  }
 
-export async function getTemperatureNow(latitude:number, longitude:number) : Promise<number> {
-    const response = await fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m`
-    )
-    if(!response.ok){
-        throw new Error("Failed to display data. Please try again later.")
-    }
+  const data = await response.json();
 
-    const data = await response.json()
+  if (
+    data.current?.temperature_2m === undefined ||
+    data.current?.wind_speed_10m === undefined ||
+    data.current?.relative_humidity_2m === undefined ||
+    data.current?.rain === undefined
+  ) {
+    throw new Error("Weather information is not available");
+  }
 
-    if(data.current?.temperature_2m === undefined){
-        throw new Error ("Information not available. Please try again later.")
-    }
-
-    return data.current.temperature_2m
+  return {
+    temperature: data.current.temperature_2m,
+    windSpeed: data.current.wind_speed_10m,
+    humidity: data.current.relative_humidity_2m,
+    rain: data.current.rain,
+  };
 }
- 
