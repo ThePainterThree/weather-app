a7ca20f7dc927520bb02cfad93e512286b389c8f
Patricia <marie.lou.trips@gmail.com>
2026-05-22 15:47:45 +0200

    add error handling on api call

--- src/api/weather-openmeteo.ts
+++ src/api/weather-openmeteo.ts
@@ -1,9 +1,20 @@
-export async function getTemperatureNow(latitude:number, longitude:number) {
+
+
+
+export async function getTemperatureNow(latitude:number, longitude:number) : Promise<number> {
     const response = await fetch(
         `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m`
     )
+    if(!response.ok){
+        throw new Error("Failed to display data. Please try again later.")
+    }
 
     const data = await response.json()
+
+    if(data.current?.temperature_2m === undefined){
+        throw new Error ("Information not available. Please try again later.")
+    }
+
     return data.current.temperature_2m
 }
  
